d726d83029933d96fc47c921243f76424611dae7
Scoutress <142579732+Scoutress@users.noreply.github.com>
2023-12-06 10:53:22 +0200

    Failų sugeneravimas

---
+++ final_project/src/test/java/lt/codeacademy/javau7/final_project/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.javau7.final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
